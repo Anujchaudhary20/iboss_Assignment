dc847a47f0d3811dfb3ce1ca7fee139c56e9f667
Anuj Kumar Singh <anuj04004@gmail.com>
2026-09-25 21:43:48 +0530

    fix login issue

--- src/main/java/com/assingment/iboss/model/auth/LoginRequest.java
+++ src/main/java/com/assingment/iboss/model/auth/LoginRequest.java
@@ -1,15 +1,17 @@
 package com.assingment.iboss.model.auth;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 @Data
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class LoginRequest {
+public class LoginRequest implements Serializable {
+    private static final long serialVersionUID = 1L;
+
     private String username;
     private String password;
 }
